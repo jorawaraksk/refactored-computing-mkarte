7e2c0ed140e086cdb908ebe4bd6d8228a2081225
jorawaraksk <jorawarzu@proton.me>
2026-03-31 19:38:25 +0530

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,30 @@
+#    This file is part of the Compressor distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
+
+from decouple import config
+
+try:
+    APP_ID = config("APP_ID", cast=int)
+    API_HASH = config("API_HASH")
+    BOT_TOKEN = config("BOT_TOKEN")
+    DEV = config("DEV", default=5868426717, cast=int)
+    OWNER = config("OWNER" , "5868426717")
+    ffmpegcode = ["-preset veryfast -c:v libx264 -b:a 64k -crf 38 -map 0 -c:s copy"]
+    THUMB = config("THUMBNAIL" , "https://graph.org/file/1cc8d7082dc910c0ccee8.jpg")
+except Exception as e:
+    LOGS.info("Environment vars Missing")
+    LOGS.info("something went wrong")
+    LOGS.info(str(e))
+    exit(1)
